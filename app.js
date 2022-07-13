efe670e861f4e4a9dc1f963fb8e9392502553711
Ranffi Ramirez <65202373+Ranffi@users.noreply.github.com>
2022-07-13 13:33:32 -0400

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,11 @@ app.use(express.static('public'));
 
 seed();
 
-app.get('/sauces', async (req, res) => {
+app.get('/'), async (_req, res) => {
+    res.redirect('/sauces')
+}
+
+app.get('/sauces', async (_req, res) => {
     const sauces= await Sauce.findAll()
     res.render('sauces', {sauces}); //points to sauces handlebar
 })
